338eb69872397900fb4d792cd2b661649792fab3
Subha-5 <subhasadhu5@gmail.com>
2023-01-26 01:10:29 +0530

    Listing NFTs for Sale

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -3,20 +3,27 @@ import logo from "../../assets/logo.png";
 import { Actor, HttpAgent } from '@dfinity/agent'
 import { idlFactory } from "../../../declarations/nft";
 import { Principal } from '@dfinity/principal';
+import Button from "./Button";
+import { opend } from "../../../declarations/opend";
 
 function Item(props) {
 
   const [name, setName] = useState();
   const [owner, setOwner] = useState();
   const [image, setImage] = useState();
+  const [button, setButton] = useState();
+  const [priceInput, setPriceInput] = useState();
 
   const id = props.id;
 
   const localHost = "http://localhost:8080/";
   const agent = new HttpAgent({ host: localHost });
+  // TODO: when deploy live, remove the following line
+  agent.fetchRootKey();
+  let NFTActor;
 
   async function loadNFT() {
-    const NFTActor = await Actor.createActor(idlFactory, {
+    NFTActor = await Actor.createActor(idlFactory, {
       agent,
       canisterId: id,
     });
@@ -30,30 +37,61 @@ function Item(props) {
     setName(name);
     setOwner(owner.toText());
     setImage(image);
+
+    setButton(<Button handleClick={handleSell}  text={"Sell"}/>);
   }
+
 
   useEffect(() => {
     loadNFT();
   }, []);
 
-    return (
-      <div className="disGrid-item">
-        <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
-          <img
-            className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-            src={image}
-          />
-          <div className="disCardContent-root">
-            <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-              {name}<span className="purple-text"></span>
-            </h2>
-            <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-              Owner: {owner}
-            </p>
-          </div>
+  let price;
+  function handleSell() {
+    // console.log("Sell Clicked!!!");
+    setPriceInput(
+      <input
+        placeholder="Price in DANG"
+        type="number"
+        className="price-input"
+        value={price}
+        onChange={(e) => price = e.target.value}
+      />);
+    setButton(<Button handleClick={sellItem} text={"Confirm"} />)
+
+  }
+
+  async function sellItem() {
+    // console.log("set price = ", price);
+    const listingResult = await opend.listItem(props.id, Number(price));
+    console.log("listing: " + listingResult);
+    if(listingResult == "Success"){
+      const openDID = await opend.getOpenDCanisterID();
+      const transferResult = await NFTActor.transferOwnership(openDID);
+      console.log("trasfer: " + transferResult );
+    }
+  }
+
+  return (
+    <div className="disGrid-item">
+      <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
+        <img
+          className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
+          src={image}
+        />
+        <div className="disCardContent-root">
+          <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
+            {name}<span className="purple-text"></span>
+          </h2>
+          <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
+            Owner: {owner}
+          </p>
+          {priceInput}
+          {button}
         </div>
       </div>
-    );
+    </div>
+  );
 
 }
 
