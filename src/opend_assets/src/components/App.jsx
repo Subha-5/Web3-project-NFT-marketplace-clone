fa7abe0951f0accd4a7986377105db1d86afdd11
Subha-5 <subhasadhu5@gmail.com>
2023-01-26 00:00:45 +0530

    Displaying Owned NFTs Using the React Router

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../../assets/home-img.png";
 import Item from "./Item";
 import Minter from "./Minter";
 
 function App() {
 
-  const NFTID = "jcuhx-tqeaq-aaaaa-aaaaa-c";
+  // const NFTID = "jcuhx-tqeaq-aaaaa-aaaaa-c";
 
   return (
     <div className="App">
       <Header />
-      <Minter />
+      {/* <Minter /> */}
       {/* <Item id={NFTID} /> */}
-      {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
   );
